dd08ca84240a1279ab4a056995d500b90b1f9f64
Phoenix Server <phoenix@thedutchgiant.nl>
2026-08-29 19:49:45 +0200

    VPS Phoenix sync - 2026-08-29 19:49:45 CEST

--- app/lib/discovery/importDiscoveryArmies.ts
+++ app/lib/discovery/importDiscoveryArmies.ts
@@ -528,7 +528,7 @@ async function getAvailableDates(): Promise<string[]> {
   return dates
     .filter(
       (date) =>
-        /^\\d{4}-\\d{2}-\\d{2}$/.test(
+        /^\d{4}-\d{2}-\d{2}$/.test(
           date
         )
     )
