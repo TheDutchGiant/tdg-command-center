916285cce8c3d8fff302c6a53628a6c776950f1e
Phoenix Server <phoenix@thedutchgiant.nl>
2026-09-15 14:46:26 +0200

    VPS Phoenix sync - 2026-09-15 14:46:26 CEST

--- app/lib/getCwlDisplaySeason.ts
+++ app/lib/getCwlDisplaySeason.ts
@@ -1,15 +1,20 @@
 export function getCwlDisplaySeason(): string {
   const now = new Date();
 
-  /*
-   * T/m de 10e tonen we de definitieve selectie
-   * van de vorige maand.
-   *
-   * Vanaf de 11e begint de nieuwe actieve cyclus.
-   */
-
   let year = now.getFullYear();
   let month = now.getMonth() + 1;
+
+  /*
+   * 1 t/m 10:
+   * toon de selectie van de zojuist afgelopen CWL-maand.
+   *
+   * Vanaf de 11e:
+   * toon de selectie voor de volgende CWL.
+   *
+   * Voorbeeld:
+   * 15 september 2026 -> 2026-10
+   * 5 september 2026  -> 2026-08
+   */
 
   if (now.getDate() <= 10) {
     month -= 1;
@@ -17,6 +22,13 @@ export function getCwlDisplaySeason(): string {
     if (month === 0) {
       month = 12;
       year -= 1;
+    }
+  } else {
+    month += 1;
+
+    if (month === 13) {
+      month = 1;
+      year += 1;
     }
   }
 
