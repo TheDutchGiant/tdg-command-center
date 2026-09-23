9d6fa9f26dbbe3ff5f9ac00603add2ba1a47dd95
Phoenix Server <phoenix@thedutchgiant.nl>
2026-09-23 23:26:59 +0200

    VPS Phoenix sync - 2026-09-23 23:26:59 CEST

--- app/lib/getCwlWorkingSeason.ts
+++ app/lib/getCwlWorkingSeason.ts
@@ -1,12 +1,27 @@
 export async function getCwlWorkingSeason(): Promise<string> {
   const now = new Date();
 
-  let year = now.getFullYear();
-  let month = now.getMonth() + 2;
+  /*
+   * De CWL-beheer-/aanmeldcyclus loopt van de 20e
+   * van een maand t/m de 19e van de volgende maand.
+   *
+   * Voorbeelden:
+   * 23 september 2026 -> CWL 2026-10
+   * 5 oktober 2026    -> CWL 2026-10
+   * 19 oktober 2026   -> CWL 2026-10
+   * 20 oktober 2026   -> CWL 2026-11
+   */
 
-  if (month === 13) {
-    month = 1;
-    year += 1;
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+
+  if (now.getDate() >= 20) {
+    month += 1;
+
+    if (month === 13) {
+      month = 1;
+      year += 1;
+    }
   }
 
   return `${year}-${String(month).padStart(2, "0")}`;
